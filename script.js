34615189edb7fbdd099000c43d9cedb1cc50d108
Contador Kevin Teixeira <111151434+SRKEVINTEIXEIRA@users.noreply.github.com>
2025-02-20 22:23:01 -0300

    script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault(); // Evita o envio do formulário para testar
+    alert('Mensagem enviada com sucesso!');
+});
