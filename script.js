5b63338325f4843ecf681f89da5b46840df9de5a
Contador Kevin Teixeira <111151434+SRKEVINTEIXEIRA@users.noreply.github.com>
2025-02-23 14:37:14 -0300

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('contact-form').addEventListener('submit', function(event) {
-    event.preventDefault(); // Evita o envio do formulário para testar
-    alert('Mensagem enviada com sucesso!');
-});
